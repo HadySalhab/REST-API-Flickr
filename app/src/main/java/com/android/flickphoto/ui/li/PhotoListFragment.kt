47ddb289f81087d680a554070ae398e5a1ca30ef
Hady Salhab <hadysalhab1@gmail.com>
2019-12-20 14:53:03 +1100

    adding setOnQueryTextListener to the SearchView

--- app/src/main/java/com/android/flickphoto/ui/li/PhotoListFragment.kt
+++ app/src/main/java/com/android/flickphoto/ui/li/PhotoListFragment.kt
@@ -5,8 +5,9 @@ import android.os.Bundle
 import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import com.android.flickphoto.R
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
 import com.android.flickphoto.databinding.FragmentPhotoListBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -22,6 +23,7 @@ class PhotoListFragment : Fragment() {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -36,12 +38,36 @@ class PhotoListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        photoListViewModel.query.observe(viewLifecycleOwner, Observer{query->
+            photoListViewModel.getFlickrPhotos(query)
+        })
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.fragment_photo_list,menu)
+        inflater.inflate(R.menu.fragment_photo_list, menu)
+        setUpSearchViewListener(menu)
+    }
+
+    private fun setUpSearchViewListener(menu: Menu) {
+        val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView as SearchView
+        searchView.apply {
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    photoListViewModel.changeQueryValue(query?:"")
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+
+            })
+
+
+        }
     }
 
 
--- app/src/main/java/com/android/flickphoto/ui/li/PhotoListViewModel.kt
+++ app/src/main/java/com/android/flickphoto/ui/li/PhotoListViewModel.kt
@@ -1,41 +1,52 @@
 package com.android.flickphoto.ui.li
 
+import android.text.TextUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.android.flickphoto.models.Photo
 import com.android.flickphoto.repositories.PhotoRepository
-import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 
-class PhotoListViewModel (private val photoRepository:PhotoRepository) : ViewModel() {
-
-    private var _photos =MutableLiveData<List<Photo>>()
-   val photos: LiveData<List<Photo>>
-    get() = _photos
-
-    init {
-        getFlickrPhotos()
-    }
+class PhotoListViewModel(private val photoRepository: PhotoRepository) : ViewModel() {
 
 
 
-    fun getFlickrPhotos() {
+    private val _photos = MutableLiveData<List<Photo>>()
+    val photos: LiveData<List<Photo>>
+        get() = _photos
+
+    private val _query = MutableLiveData<String>("")
+    val query:LiveData<String>
+    get() = _query
+
+
+
+
+    fun getFlickrPhotos(query:String) {
+        if(TextUtils.isEmpty(query)){
+
         viewModelScope.launch {
-               _photos.value =  photoRepository.fetchPhotos()
+            _photos.value = photoRepository.fetchPhotos()
 
         }
+        }else{
+            viewModelScope.launch {
+                _photos.value = photoRepository.searchPhotos(query)
+            }
+        }
 
+    }
+    fun changeQueryValue(query:String){
+        _query.value = query
     }
 
     override fun onCleared() {
         super.onCleared()
         viewModelScope.cancel()
     }
-
 
 
 }
